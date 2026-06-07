a426c09cc1005ea8de01855f4ba5c7b2607d968f
Yuvraj Singh <rajakandari87@gmail.com>
2026-06-07 23:15:34 +0530

    Revert "Update API base URL to use environment variable"
    This reverts commit 165cd49ddff32860d5e915d18dfdaadfe7062f25.

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
+const api = axios.create({ baseURL: '/api' });
 
-export const getTasks = () => api.get("/tasks");
-export const addTask = (data) => api.post("/tasks", data);
+export const getTasks = () => api.get('/tasks');
+export const addTask = (data) => api.post('/tasks', data);
 export const updateTask = (id, data) => api.put(`/tasks/${id}`, data);
 export const toggleTask = (id) => api.patch(`/tasks/${id}/toggle`);
 export const deleteTask = (id) => api.delete(`/tasks/${id}`);
