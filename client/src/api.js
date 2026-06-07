165cd49ddff32860d5e915d18dfdaadfe7062f25
Yuvraj Singh <rajakandari87@gmail.com>
2026-06-07 23:01:55 +0530

    Update API base URL to use environment variable

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
-export const getTasks = () => api.get('/tasks');
-export const addTask = (data) => api.post('/tasks', data);
+export const getTasks = () => api.get("/tasks");
+export const addTask = (data) => api.post("/tasks", data);
 export const updateTask = (id, data) => api.put(`/tasks/${id}`, data);
 export const toggleTask = (id) => api.patch(`/tasks/${id}/toggle`);
 export const deleteTask = (id) => api.delete(`/tasks/${id}`);
