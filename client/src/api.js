b1fe6e0b19bad0afa4ec6a505bb1df2b67c69d80
yuvraj-kandari <rajakandari87@gmail.com>
2026-06-07 23:17:42 +0530

    Configure API URL for deployment

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 export const getTasks = () => api.get('/tasks');
 export const addTask = (data) => api.post('/tasks', data);
